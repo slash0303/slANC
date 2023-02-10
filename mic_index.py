f41f7d3686fa2ebc9b71d30b58a067427d543c4c
slash <gksqks0303@gmail.com>
2023-02-10 21:16:15 +0900

    'mic_index.py' generate 'mic_index.json'file

--- mic_index.py
+++ mic_index.py
@@ -1,10 +1,20 @@
 import pyaudio
-
+from eaxtension import jsonE
+from eaxtension import LogE
 
 p = pyaudio.PyAudio()
+
+sav_text = {}
+
 for i in range(p.get_device_count()):
     desc = p.get_device_info_by_index(i)
 
     if desc["maxInputChannels"] != 0:
-        print(desc)
+        sav_text[desc["index"]] = desc
+        LogE.d("desc index", desc["index"])
+        LogE.d("desc data", desc)
+        LogE.g("sav in for", sav_text)
 
+LogE.d("sav data", sav_text)
+
+jsonE.dumps("mic_index", sav_text)
