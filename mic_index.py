db348b4bda78ba48662c29c147fd2c2f127647d0
slash <gksqks0303@gmail.com>
2023-02-10 21:04:03 +0900

    base code of mic_index.py

--- mic_index.py
+++ mic_index.py
@@ -1,8 +1,10 @@
 import pyaudio
 
+
 p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     desc = p.get_device_info_by_index(i)
 
     if desc["maxInputChannels"] != 0:
         print(desc)
+
