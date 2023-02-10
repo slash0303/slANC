67f93b94c3143da8826f190da65691fc4a8cf6e0
slash <gksqks0303@gmail.com>
2023-02-10 22:05:53 +0900

    add selecting feature which device is named "Microphone"

--- mic_index.py
+++ mic_index.py
@@ -4,17 +4,23 @@ from eaxtension import LogE
 
 p = pyaudio.PyAudio()
 
-sav_text = {}
+sav_text = {"all_input_device":{}}
+i_index = []
+mic_sav = {}
+sav_all = sav_text["all_input_device"]
 
 for i in range(p.get_device_count()):
     desc = p.get_device_info_by_index(i)
 
     if desc["maxInputChannels"] != 0:
-        sav_text[desc["index"]] = desc
-        LogE.d("desc index", desc["index"])
-        LogE.d("desc data", desc)
-        LogE.g("sav in for", sav_text)
+        sav_all[desc["index"]] = desc
+        i_index.append(desc["index"])
 
-LogE.d("sav data", sav_text)
+for i in i_index:
+    text = sav_all[i]["name"]
+    if text.find("Microphone") != -1 :
+        mic_sav[i] = sav_all[i]
+
+sav_text["only_mic"] = mic_sav
 
 jsonE.dumps("mic_index", sav_text)
