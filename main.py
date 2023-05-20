11c22cfb0a2e3c501683eba3dddbcb5f2600e399
slash <gksqks0303@gmail.com>
2023-05-21 01:45:20 +0900

    add audio_fft function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pyaudio
 import numpy as np
 from numpy import fft
@@ -21,15 +22,31 @@ stream = p.open(frames_per_buffer=CHUNK,
                 input=INPUT,
                 input_device_index=0,)
 
-# main
-while True:
-    start = t.time()
+def audio_fft():
+    # start = t.time()
     data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
     n = len(data)
     x = np.linspace(0, 44100 / 2, int(n / 2))
     y = fft.fft(data) / n
     y = np.abs(y)
     y = y[range(int(n / 2))]
-    end = t.time()
-    cal_time = end - start
-    print(cal_time)
+    # end = t.time()
+    # cal_time = end - start
+    # print(cal_time)
+
+    return {"x": x, "y": y}
+
+# main
+while True:
+    fft_data = audio_fft()
+    for y in fft_data["y"]:
+        if y > 100:
+            print(list(fft_data["y"]).index(y))
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_xlim(0, 10000)
+    ax.set_ylim(0, 1000)
+    ax.plot(fft_data["x"], fft_data["y"])
+
+    plt.show()
